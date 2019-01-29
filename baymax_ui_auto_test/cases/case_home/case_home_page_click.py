9b7dd0c040603e2027a6a33201f1b3d1cf257112
wanganchong <9262623@qq.com>
2019-01-29 17:36:01 +0800

    添加了home页用例 作业模板、作业管理、作业运维

--- baymax_ui_auto_test/cases/case_home/case_home_page_click.py
+++ baymax_ui_auto_test/cases/case_home/case_home_page_click.py
@@ -70,7 +70,7 @@ class HomePageTest(ParametrizedTestCase):
         page.operate()
         page.check_point()
 
-    # 校验 “数据治理--血缘分析”
+    # 校验 “数据治理--流程管理”
     def test_a009_flow_manage(self):
         app = {"logTest": self.logTest, "driver": self.driver, "path": PATH("../YAML/home/流程管理.yaml"),
                "caseName": sys._getframe().f_code.co_name}
@@ -102,9 +102,33 @@ class HomePageTest(ParametrizedTestCase):
         page.operate()
         page.check_point()
 
-        # 校验 “数据监控--任务监控.yaml”
+    # 校验 “数据监控--任务监控.yaml”
     def test_a013_task_control(self):
         app = {"logTest": self.logTest, "driver": self.driver, "path": PATH("../YAML/home/任务监控.yaml"),
+               "caseName": sys._getframe().f_code.co_name}
+        page = HomePage(app)
+        page.operate()
+        page.check_point()
+
+    # 校验 “实时计算--作业管理.yaml”
+    def test_a014_work_manage(self):
+        app = {"logTest": self.logTest, "driver": self.driver, "path": PATH("../YAML/home/作业管理.yaml"),
+               "caseName": sys._getframe().f_code.co_name}
+        page = HomePage(app)
+        page.operate()
+        page.check_point()
+
+    # 校验 “实时计算--作业运维.yaml”
+    def test_a015_work_operations(self):
+        app = {"logTest": self.logTest, "driver": self.driver, "path": PATH("../YAML/home/作业运维.yaml"),
+               "caseName": sys._getframe().f_code.co_name}
+        page = HomePage(app)
+        page.operate()
+        page.check_point()
+
+    # 校验 “实时计算--作业模板.yaml”
+    def test_a016_work_template(self):
+        app = {"logTest": self.logTest, "driver": self.driver, "path": PATH("../YAML/home/作业模板.yaml"),
                "caseName": sys._getframe().f_code.co_name}
         page = HomePage(app)
         page.operate()
