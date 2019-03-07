55d431b7903f5b0590f8e165c75b3fb7f494ac44
wanganchong <9262623@qq.com>
2019-03-07 20:54:47 +0800

    添加了采集器模块的用例

--- baymax_ui_auto_test/runner/runner.py
+++ baymax_ui_auto_test/runner/runner.py
@@ -7,6 +7,8 @@ from cases.case_login import LoginTest
 from cases.case_data_integration.case_resourceMan.case_operate_dir import OperateDirTest, OperateDirTest2
 from cases.case_data_integration.case_data_import import DataImportTest, DataImportTest2
 from cases.case_data_integration.case_file_management import FileManagementTest
+from cases.case_data_integration.case_collector.case_collector import CollectorTemplateTest, CollectorimportDataTest, CollectorTaskListTest
+from cases.case_data_integration.case_file_import.case_file_import import FileImportTest
 import unittest
 from datetime import datetime
 from common.TearDown import mk_file
@@ -23,6 +25,11 @@ def runnerCaseApp():
     suite.addTest(ParametrizedTestCase.parametrize(DataImportTest))
     suite.addTest(ParametrizedTestCase.parametrize(DataImportTest2))
     suite.addTest(ParametrizedTestCase.parametrize(FileManagementTest))
+    suite.addTest(ParametrizedTestCase.parametrize(FileImportTest))
+    suite.addTest(ParametrizedTestCase.parametrize(CollectorTemplateTest))
+    suite.addTest(ParametrizedTestCase.parametrize(CollectorimportDataTest))
+    suite.addTest(ParametrizedTestCase.parametrize(CollectorTaskListTest))
+
     unittest.TextTestRunner(verbosity=2).run(suite)
     end_time = datetime.now()
     countDate(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), str((end_time - start_time).seconds) + "秒")
@@ -31,4 +38,4 @@ if __name__ == '__main__':
     mk_file()
     runnerCaseApp()
     writeExcel()
-    send()
+    # send()
